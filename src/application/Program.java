903ab889ba5c0c107512080e171c1d532fef8fe2
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2020-05-25 07:39:48 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted: New Id = "+newSeller.getId());
 		
+		System.out.println("\n====Test #5: seller - update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
 	}
 
 }
