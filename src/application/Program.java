68f907e22c91269ddff4567be4f2636c002b348a
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2020-05-25 07:31:12 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,6 +32,11 @@ public class Program {
 			System.out.println(s);
 		}
 		
+		System.out.println("\n====Test #4: seller - insert ====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted: New Id = "+newSeller.getId());
+		
 	}
 
 }
